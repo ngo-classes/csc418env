d2fb7f7c59f1fb577bc51a78716c72ed4e1643e0
Linh B Ngo <lngo@wcupa.edu>
2024-11-07 16:37:39 -0500

    Update start-master.sh

--- docker/master/start-master.sh
+++ docker/master/start-master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /working_dir && jupyter lab --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 --allow-root &
-code-server --port 8088
+code-server --bind-addr 0.0.0.0:8088
 
