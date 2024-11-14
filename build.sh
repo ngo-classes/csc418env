402bdb414dc7c30a6586426cf22621087f64f313
Linh B Ngo <lngo@wcupa.edu>
2024-11-14 16:04:39 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-docker system prune -a --volumes
+docker image rm master:latest
+docker image rm worker:latest
 
 docker build -t master:latest ./docker/master
 docker build -t worker:latest ./docker/worker
