666671e0a6d02c8f5b1fb2c86ecf54db0aaa4b51
Huy Nguyen <huy55465@gmail.com>
2025-01-10 00:52:18 -0500

    change mongodb install and conf copy order
    Otherwise dpkg (or more like, mongod) will complain. Because the thing
    tries to generate a default config file.
    
    commit-by: Huy Nguyen

--- docker/worker/install.sh
+++ docker/worker/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Install Mongo for Debian
-curl -fSsL "https://repo.mongodb.org/apt/debian/dists/bookworm/mongodb-org/8.0/main/binary-amd64/mongodb-org-mongos_${MONGO_VER}_amd64.deb" -o ./mongodb.deb
+curl -fSsL "https://repo.mongodb.org/apt/debian/dists/bookworm/mongodb-org/8.0/main/binary-amd64/mongodb-org-server_${MONGO_VER}_amd64.deb" -o ./mongodb.deb
 dpkg -i ./mongodb.deb
